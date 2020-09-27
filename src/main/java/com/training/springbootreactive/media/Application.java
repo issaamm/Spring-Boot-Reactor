ce37619a156d408f7beb1f32e1e841045e9f3914
issam bouzid <issam.fpn.info@gmail.com>
2020-09-27 20:09:47 +0100

    fix package naming

--- src/main/java/com/training/springbootreactive/media/Application.java
+++ src/main/java/com/training/springbootreactive/media/Application.java
@@ -1,4 +1,4 @@
-package com.training.springbootreactive.media.springbootreactive.media;
+package com.training.springbootreactive.media;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
